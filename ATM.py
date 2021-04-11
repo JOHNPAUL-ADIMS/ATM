39774a7094e1137a296c47025c5b65999b676e8d
JOHNPAUL ADIMONYEMMA <johnpauladimonyemma@gmail.com>
2021-04-12 00:56:07 +0100

    Added exceptions

--- ATM.py
+++ ATM.py
@@ -4,6 +4,7 @@ from datetime import time
 from os import P_NOWAIT
 from babel import numbers
 import random
+import time
 from datetime import datetime
 now = datetime.now()
 currentDate = now.strftime("%d-%B-%Y")
@@ -20,25 +21,42 @@ database2 = {}
 def generateAccountNumber ():
     return random.randrange(1111111111,9999999999)
 def initalization():
-    print("Welcome to the BANK OF JP")
-    ownAccount = int(input("Do you have an account with us?\n If (yes) enter 1\n If (no) enter 2. \n Enter your Option  \n "))
-    if (ownAccount == 1):
-        
-        login()
-    elif(ownAccount == 2):
-       
-        print(registeration())
-    else:
-        print("You have selected invalid option")
+    print("Welcome to the BANK OF JP".center(10, '*').upper())
+    print("""
+        Do you have an account with us ?
+            1. Yes
+            2. No
+    """)
+    
+    ownAccount = int(input("Enter your option>>>   "))
+    
+    try:
+        if (ownAccount == 1):
+            login()
+        elif(ownAccount == 2):
+            print(registeration())
+        else:
+            print("You have selected invalid option")
+    except ValueError:
+        print('Invalid option: Try again later')
+        return initalization()
+
     initalization()
 
 
 def registeration():
-    print('=== CLICK BELOW TO REGISTER ===')
-    email = input("Enter your email address\n")
-    firstName = input("Enter your first name\n")
-    lastName = input("Enter your last name\n")
-    password = input ("Create a password\n")
+    print('=== REGISTER BELOW ===')
+
+    try:
+        email = input("Enter your email address\t\n")
+        firstName = input("Enter your first name\t\n")
+        lastName = input("Enter your last name\t\n")
+        password = input ("Create a password\t\n")
+
+    except NameError:
+        print('Invalid option: Try again later')
+        return registeration()
+
     accountNumber = generateAccountNumber()
 
     database[accountNumber] = [firstName, lastName, email, password]
@@ -55,66 +73,120 @@ def login():
     userAccountNumber = int(input("Enter your account number \n"))
     password = input("Enter your password\n")
 
-    for accountNumber, userDetails in database.items():
-        if (userAccountNumber == accountNumber):
-            if(userDetails[3] == password):
-                
-                print(f'==== WELCOME {userDetails[0]} to JP BANK ')
-             # importing date and time
-                print("Today's date is == :", currentDate)
-                print("Your time is ==", currentTime)
-                print("********************************")
-                print("********************************")
-                bankingOperation(userDetails)
-            # else:
-            #     print("Invalid account or password, Enter your correct account Number")
-            #     login()
-    print('Invalid account or password')
-    login()
-    
+    try:
+
+        for accountNumber, userDetails in database.items():
+            if (userAccountNumber == accountNumber):
+                if(userDetails[3] == password):
+                    
+                    print(f'==== WELCOME {userDetails[0]} to JP BANK ')
+                # importing date and time
+                    print("Today's date is == :", currentDate)
+                    print("Your time is ==", currentTime)
+                    print("********************************")
+                    print("********************************")
+                    bankingOperation(userDetails)
+                else:
+                    print("Invalid account or password, Enter your correct account Number")
+                    login()
+        # print('Invalid account or password')
+        # login()
+    except ValueError:
+        print('Invalid option: Try again later')
+    return login()
+
+
 def bankingOperation(user):
     print(f"Welcome {user[0]}")
-    optionSelected =  int(input("What will you want to do\n (1) Deposit\n (2) Withdrawal\n (3)Complaint\n (4) Logout\n (5)exit\n"))
-    if(optionSelected == 1):
-        depositOperation()
-    elif(optionSelected == 2):
-        withdrawalOperation()
-    elif(optionSelected == 3):
-        complaintOperation ()
-    elif (optionSelected == 4):
-        logout()
-    elif(optionSelected == 5):
-        exit()
-    else:
-        print("Invalid option selected")
-        bankingOperation(user)
+    print(""" What will you want to do 
+        1. Deposit
+        2. Withdrawal
+        3. Complaint
+        4. Logout
 
-def withdrawalOperation():
+    """)
+    try:
+        optionSelected =  int(input(">>  "))
+    except ValueError:
+        print('Invalid option: Try again later')
+        return bankingOperation()
+    try:
+        if(optionSelected == 1):
+            depositOperation()
+        elif(optionSelected == 2):
+            withdrawalOperation()
+        elif(optionSelected == 3):
+            complaintOperation ()
+        elif (optionSelected == 4):
+            logout()
+        else:
+            print("Invalid option selected")
+            bankingOperation(user)
+
+    except ValueError:
+        print('Invalid option: Try again later')
+        return bankingOperation()
+    return bankingOperation()
+
+def withdrawalOperation(user):
     print("How much would you like to withdraw?\n")
     print("********************************")
-    withdraw = float(input("Enter your amount\t "))
-    print("********************************")
-    print("Take your cash, %d" %withdraw)
 
-def depositOperation():       
+    try:
+        withdraw = float(input("Enter your amount\t "))
+        print("********************************")
+        print("Take your cash, %d" %withdraw)
+
+    except ValueError:
+        print('Invalid option: Try again later')
+        return withdrawalOperation(user)
+    return bankingOperation(user)
+
+def depositOperation(user):       
     print("How much will you like to deposit?\n")
     print("********************************")
-    deposit = float(input("Enter your current deposit\n"))
-    print("Your Current Balance is %d " %deposit)
-    print("********************************")
 
+    try:
+        deposit = float(input("Enter your current deposit\n"))
+        print("Your Current Balance is %d " %deposit)
+        print("********************************")
+    except ValueError:
+        print('Invalid option: Try again later')
+        return depositOperation(user)
+    return bankingOperation(user)
 
-def complaintOperation():
+def complaintOperation(user):
     print("What issue will you like to report")
     print("********************************")
     complaint = input("Enter your complaint\n")
-    # database2 = [Complaint]  for 
+        # database2 = [Complaint]  for 
     print(" Thank you for contacting us")
     print("********************************")
+    return bankingOperation(user)
 
 
 def logout():
-    login()
+    print(f'Do you want to exit?')
+    print(f'If Yes press 1 or No press 2')
+    try:
+        exitoption = int(input('>>  '))
+        time.sleep(0.5)
+    except ValueError:
+        print("You have made an invalid selection. Try again\n")
+        return exit()
+    try:
+
+        if exitoption == 1:
+            print("We will love to see you back")
+            exit()
+
+        elif exitoption == 2:
+            return bankingOperation()
+        else:
+            return logout()
+    except ValueError:
+        print('Invalid Selection. Try again')
+    return logout()
 
 
 
